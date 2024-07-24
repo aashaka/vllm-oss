9db31f708d8998e9ca1c59692a7a14f46446cb5a
Aashaka Shah <aashaka96@gmail.com>
2024-07-24 06:29:09 +0000

    Finish implementing MSCCL++ based allreduce for vLLM
    Chooses the best algorithm and kernel configuratios for a given input size

--- vllm/model_executor/parallel_utils/mscclpp_allreduce.py
+++ vllm/model_executor/parallel_utils/mscclpp_allreduce.py
@@ -7,10 +7,10 @@ from vllm.model_executor.parallel_utils.communication_op import broadcast
 from vllm.model_executor.parallel_utils.parallel_state import (
     get_tensor_model_parallel_group, get_msccl_tensor_model_parallel_group, get_msccl_tensor_model_parallel_rank)
 
-import netifaces as ni
-import ipaddress
-from mpi4py import MPI
-import mscclpp.comm as mscclpp_comm
+# import netifaces as ni
+# import ipaddress
+# from mpi4py import MPI
+# import mscclpp.comm as mscclpp_comm
 
 max_elements = 33554432
 
@@ -65,16 +65,16 @@ def find_best_config(mscclpp_call, niter, *args):
             best_time = cur_time
             best_config = config
 
-    best_config = MPI.COMM_WORLD.bcast(best_config, root=0)
-    if MPI.COMM_WORLD.rank == 0:
-        print(best_config, end="", flush=True)
+    # for MPI test
+    # best_config = MPI.COMM_WORLD.bcast(best_config, root=0)
+    # if MPI.COMM_WORLD.rank == 0:
+    #     print(best_config, end="", flush=True)
 
     # for actual run
-    # best_config_tensor = torch.tensor(best_config, dtype=torch.int32, device=torch.cuda.current_device())
-    # best_config_tensor = broadcast(best_config_tensor, src=0, group=get_tensor_model_parallel_group())
-    # best_config = best_config_tensor.cpu().tolist()
-    # if get_msccl_tensor_model_parallel_rank() == 0:
-    #     print(best_config, end="", flush=True)
+    best_config_tensor = torch.tensor(best_config, dtype=torch.int32, device=torch.cuda.current_device())
+    best_config_tensor = broadcast(best_config_tensor, src=0, group=get_tensor_model_parallel_group())
+    best_config = best_config_tensor.cpu().tolist()
+
     return best_config, best_time
 
 def find_best_algo_config(mscclpp_algos, niter):
@@ -82,6 +82,7 @@ def find_best_algo_config(mscclpp_algos, niter):
     best_time = 10000000.0
     best_algo = None
     best_config = None
+    best_algo_id = None
     for i, algo in enumerate(mscclpp_algos):
         if algo is None:
             continue
@@ -90,33 +91,44 @@ def find_best_algo_config(mscclpp_algos, niter):
             best_time = cur_time
             best_algo = algo
             best_config = config
-            algo.set_params(*config)
-    if MPI.COMM_WORLD.rank == 0:
-        print(best_algo, end="", flush=True)
+            best_algo_id = i
+    # for MPI test
+    # if MPI.COMM_WORLD.rank == 0:
+    #     print(best_algo, end="", flush=True)
+    # for actual run
     # if get_msccl_tensor_model_parallel_rank() == 0:
     #     print(best_algo, end="", flush=True)
-    return i, best_algo, best_config
+    return best_algo_id, best_algo, best_config
 
 class MscclppAllReduce:
+    # Buffers used as input and output for MSCCL++ allreduce
     all_reduce_buff = {torch.float16: None, torch.float32: None, torch.int: None}
     all_reduce_buff_out = {torch.float16: None, torch.float32: None, torch.int: None}
-    # all_reduce_buff_ptr = {torch.float16: None, torch.float32: None, torch.int: None}
+
+    # Cache for allreduce kernels
     ar_kernel = {}
+
+    # Per-size best algo and config
     best_configs = {}
+
     BUILT_KERNELS = False
 
     @classmethod
-    def build_kernels(cls, data_type, mscclpp_group):
+    def build_kernels(cls, data_type): #, mscclpp_group):
+        # Only build kernels once
         if cls.BUILT_KERNELS:
             return
-        # mscclpp_group = get_msccl_tensor_model_parallel_group()
 
-        # cls.all_reduce_buff[data_type] = cp.zeros(max_elements, dtype=torch_to_cupy_type(data_type))
+        mscclpp_group = get_msccl_tensor_model_parallel_group()
+
         cls.all_reduce_buff[data_type] = torch.zeros(max_elements, dtype=data_type, device=torch.cuda.current_device())
         cls.all_reduce_buff_out[data_type] = torch.zeros(max_elements, dtype=data_type, device=torch.cuda.current_device())
         cp.cuda.runtime.deviceSynchronize()
 
         proxy_service = ProxyService()
+        # Compile kernels for different allreduce algorithms using the same input buffer
+        # These algorithms are for shards=8
+        # AllReduce2 uses a lot more memory, and hence we limit the number of elements to 1M
         mscclpp_algos = [
             MscclppAllReduce1(mscclpp_group, cls.all_reduce_buff[data_type]),
             MscclppAllReduce2(mscclpp_group, cls.all_reduce_buff[data_type][:1048576], cls.all_reduce_buff_out[data_type][:1048576]),
@@ -129,82 +141,81 @@ class MscclppAllReduce:
 
 
     @classmethod
-    def get_best_ar_kernel(cls, nelem, data_type, mscclpp_group):
+    def get_best_ar_kernel(cls, nelem, data_type):
         assert nelem <= max_elements, f"AllReduce size is too large {nelem}"
-        # data_type_str = type_to_str(data_type)
         if (nelem, data_type) in cls.best_configs:
+            # if the best config is already found for this data size, return the kernel
             algo_offset, config = cls.best_configs[(nelem, data_type)]
-            mscclpp_call = cls.ar_kernel[(nelem, data_type)][algo_offset]
-            mscclpp_call.memory = mscclpp_call.memory[:nelem]
-            mscclpp_call.set_params(*config)
+            mscclpp_call = cls.ar_kernel[data_type][algo_offset]
+            mscclpp_call.memory = cls.all_reduce_buff[data_type][:nelem]
         else:
-            # dtype_str = type_to_str(data_type)
             mscclpp_algos = [algo for algo in cls.ar_kernel[data_type]]
             for algo in mscclpp_algos:
                 algo.memory = cls.all_reduce_buff[data_type][:nelem]
             if nelem * algo.memory.element_size()  >= 2**20:
                 mscclpp_algos[1] = None
-            # mscclpp_group = get_msccl_tensor_model_parallel_group()
-            mscclpp_group.barrier()
+            # Get best combination of algorithm and config
             algo_id, mscclpp_call, config = find_best_algo_config(mscclpp_algos, 20)
+            # Cache the best config for this size (closest power of 2) and data type
             cls.best_configs[(nelem, data_type)] = (algo_id, config)
-        
+
+        mscclpp_call.set_params(*config)
         return mscclpp_call
 
     @classmethod
-    def mscclpp_allreduce(cls, input_: torch.Tensor, mscclpp_group):
-        cls.build_kernels(input_.dtype, mscclpp_group)
+    def mscclpp_allreduce(cls, input_: torch.Tensor): #, mscclpp_group):
         nelem = input_.numel()
-        # nbytes = nelem * input_.element_size()
-        mscclpp_ar_call = cls.get_best_ar_kernel(nelem, input_.dtype, mscclpp_group)
-        # cupy_ptr = mscclpp_ar_call.memory.data.ptr
-        # torch_ptr = input_.data_ptr()
+        cls.build_kernels(input_.dtype) #, mscclpp_group)
+        mscclpp_ar_call = cls.get_best_ar_kernel(nelem, input_.dtype)
+        # print(f"shape is : {input_.shape} nelem = {nelem} call.memory = {mscclpp_ar_call.memory.shape}", flush=True)
         mscclpp_ar_call.memory.copy_(input_.reshape(-1), non_blocking=True)
-        # cp.cuda.runtime.memcpyAsync(cupy_ptr, torch_ptr, nbytes, 3)
         if hasattr(mscclpp_ar_call, "memory_out"):
-        #     dummy_output = torch.zeros(nelem, device=torch.cuda.current_device(), dtype=input_.dtype)
             mscclpp_ar_call.memory_out = cls.all_reduce_buff_out[input_.dtype][:nelem]
-        return mscclpp_ar_call(torch.cuda.current_stream().cuda_stream).reshape(input_.shape)
+        return mscclpp_ar_call(torch.cuda.current_stream()).reshape(input_.shape)
 
-def is_valid(ip):
-    """
-    Check if the IP address is valid for connecting to other devices.
-    This excludes loopback (127.0.0.1) and link-local (169.254.x.x) addresses.
-    """
-    ip_obj = ipaddress.ip_address(ip)
-    return not (ip_obj.is_loopback or ip_obj.is_link_local or ip_obj.is_multicast)
+# def is_valid(ip):
+#     """
+#     Check if the IP address is valid for connecting to other devices.
+#     This excludes loopback (127.0.0.1) and link-local (169.254.x.x) addresses.
+#     """
+#     ip_obj = ipaddress.ip_address(ip)
+#     return not (ip_obj.is_loopback or ip_obj.is_link_local or ip_obj.is_multicast)
 
 
-def get_netinterface_info():
-    """
-    Returns the name of the first network interface with a valid IP address that it finds.
-    """
-    interfaces = ni.interfaces()
-    for interface in interfaces:
-        addresses = ni.ifaddresses(interface)
-        if ni.AF_INET in addresses:
-            for addr in addresses[ni.AF_INET]:
-                ip_address = addr["addr"]
-                if is_valid(ip_address):
-                    print(f"Selected Interface: {interface}, IP Address: {ip_address}")
-                    return interface, ip_address
-    return None, None
+# def get_netinterface_info():
+#     """
+#     Returns the name of the first network interface with a valid IP address that it finds.
+#     """
+#     interfaces = ni.interfaces()
+#     for interface in interfaces:
+#         addresses = ni.ifaddresses(interface)
+#         if ni.AF_INET in addresses:
+#             for addr in addresses[ni.AF_INET]:
+#                 ip_address = addr["addr"]
+#                 if is_valid(ip_address):
+#                     print(f"Selected Interface: {interface}, IP Address: {ip_address}")
+#                     return interface, ip_address
+#     return None, None
 
-if __name__ == "__main__":
-    shm_comm = MPI.COMM_WORLD.Split_type(MPI.COMM_TYPE_SHARED, 0, MPI.INFO_NULL)
-    N_GPUS_PER_NODE = shm_comm.size
-    shm_comm.Free()
-    cp.cuda.Device(MPI.COMM_WORLD.rank % N_GPUS_PER_NODE).use()
+# if __name__ == "__main__":
+#     shm_comm = MPI.COMM_WORLD.Split_type(MPI.COMM_TYPE_SHARED, 0, MPI.INFO_NULL)
+#     N_GPUS_PER_NODE = shm_comm.size
+#     shm_comm.Free()
+#     cp.cuda.Device(MPI.COMM_WORLD.rank % N_GPUS_PER_NODE).use()
 
-    # create a MscclppGroup
-    network_interface, my_ip = get_netinterface_info()
-    root_ip = MPI.COMM_WORLD.bcast(my_ip, root=0)
-    ifIpPortTrio = network_interface + ":" + root_ip + ":50000"  # some random port
-    mscclpp_group = mscclpp_comm.CommGroup(
-        interfaceIpPortTrio=ifIpPortTrio, rank=MPI.COMM_WORLD.rank, size=MPI.COMM_WORLD.size
-    )
+#     # stream = torch.cuda.Stream()
+#     # torch.cuda.set_stream(stream)
 
-    input_ = torch.ones(1, 10, device="cuda", dtype=torch.float16)
-    output = MscclppAllReduce.mscclpp_allreduce(input_, mscclpp_group)
-    torch.cuda.synchronize()
-    print(output)
+#     # create a MscclppGroup
+#     network_interface, my_ip = get_netinterface_info()
+#     root_ip = MPI.COMM_WORLD.bcast(my_ip, root=0)
+#     ifIpPortTrio = network_interface + ":" + root_ip + ":50000"  # some random port
+#     mscclpp_group = mscclpp_comm.CommGroup(
+#         interfaceIpPortTrio=ifIpPortTrio, rank=MPI.COMM_WORLD.rank, size=MPI.COMM_WORLD.size
+#     )
+
+#     input_ = torch.ones(128,  device="cuda", dtype=torch.float16)
+#     output = MscclppAllReduce.mscclpp_allreduce(input_, mscclpp_group)
+#     torch.cuda.synchronize()
+#     print(output)
+#     assert torch.allclose(output, torch.ones(128, device="cuda", dtype=torch.float16) * MPI.COMM_WORLD.size)
